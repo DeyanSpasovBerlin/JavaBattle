7c44d9a277aa9ca288c011ebb834d5eff99d2e0a
DeyanSpasovBerlin <pislpe@gmail.com>
2024-10-09 21:47:41 +0200

    Delete BattleGame directory

--- BattleGame/MultipleBattle.java
+++
@@ -1,8 +0,0 @@
-package BattleGame;
-
-public class MultipleBattle {
-    public static void main(String[] args) {
-        SetOfCharacters instans = new SetOfCharacters();
-        instans.setCharacters();
-    }
-}
